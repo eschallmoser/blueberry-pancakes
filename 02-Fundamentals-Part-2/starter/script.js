1b81b23da9bc281aba7f0ada31e424ad8cba4b81
Erin Schallmoser <dialogofadream@gmail.com>
2021-11-15 09:01:28 -0800

    build functions to: calculate age, years until retirement, average scores, population percentages. Build arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -88,6 +88,166 @@
 //-------------------------------------------------------------------------
 
 //Arrow Functions:
+
+//special form of function expression that is shorter and therefore faster to write
+
+//arrow function:               //implicit return
+//simplest form
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3); //46
+
+// //gets more complex when we add more parameters and/or more code
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   //   return retirement; //can't do implicit since we have more code here
+//   return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1980, "Bob"));
+
+//-------------------------------------------------------------------------
+
+//Functions Calling Other Functions:
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+//-------------------------------------------------------------------------
+
+//Reviewing Functions:
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired`);
+//     return -1; //shows that the number actually has no meaning
+//   }
+
+//   //return keyword--first it returns the value as it's instructed, but then it exits the functions immediately
+// };
+
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
+
+//ALL functions can receive data, transform data, and return data
+
+//--------------------------------------------------------------------------
+
+//Introduction to Arrays:
+
+//how we would have to do it without arrays
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
+
+// //an array is a data structure, like a big container into which we can throw variables, and reference them in the future
+
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
+
+// //another way to write arrays
+// const years = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]); //Michael
+// console.log(friends[2]); //Peter
+
+// console.log(friends.length); //gives us the amount of elements in the array
+
+// //we can use .length to get the last element of an array
+// console.log(friends[friends.length - 1]);
+
+// //we can mutate the contents of the array, no matter if it's const or let, because it isn't a primitive data type
+// friends[2] = "Jay";
+// console.log(friends); //Jay has replaced Peter
+
+// //but we can't replace the entire array this way
+// // friends = ["Bob", "Alice"]; //throws an error message: assignment to constant variable
+
+// //an array can hold values of different types at the same time
+// //we can even put other arrays inside of array
+// const firstName = "Jonas";
+// const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+// console.log(jonas);
+
+//Exercise
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// // console.log(calcAge(years)); //this won't work, becquse it's an array, so it's like asking the code to subtract an array from 2037, gives us NaN
+
+// //we can still use the function on individual array elements though
+
+// const age1 = calcAge(years[0]); //function is called with 1990
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// //since we started with an array, it's a nice idea to end with an array as well
+// //we can place function calls into an array
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[years.length - 1]),
+// ];
+// console.log(ages); //the same values from line 211, but now they're placed in an array
+
+//--------------------------------------------------------------------------
+
+//Basic Array Operations (Methods)
+
+//methods are functions that you can apply directly to an array
+
+const friends = ["Michael", "Steven", "Peter"];
+
+//we can add an element to the end of an array
+// friends.push("Jay");
+console.log(friends);
+
+//push can return a value as well
+const newLength = friends.push("Jay");
+console.log(newLength); //4
+
+//we can add an element to the beginning of an array
+friends.unshift("John");
+console.log(friends);
+
+//we can remove an element from the end of an array
+console.log(friends.pop()); //don't need to pass in an argument, it's going to take the last element
+//pop returns the removed element as its value--Jay
+console.log(friends); //Jay is gone
+
+//we can remove an element from the beginning of an array
+friends.shift(); //first
+console.log(friends); //John is gone
+
 //-------------------------------------------------------------------------
 
 //ASSIGNMENTS
@@ -103,3 +263,77 @@
 // console.log(describeCzech);
 // console.log(describeFrance);
 // console.log(describeNigeria);
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+
+// const popOfUSA = percentageOfWorld1(329.5);
+// const popOfNigeria = percentageOfWorld1(206.1);
+// const popOfVietnam = percentageOfWorld1(97.34);
+// console.log(popOfUSA, popOfNigeria, popOfVietnam);
+
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+
+// const popOfUSA = percentageOfWorld2(329.5);
+// const popOfNigeria = percentageOfWorld2(206.1);
+// const popOfVietnam = percentageOfWorld2(97.34);
+// console.log(popOfUSA, popOfNigeria, popOfVietnam);
+
+// const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+// const popOfUSA = percentageOfWorld3(329.5);
+// const popOfNigeria = percentageOfWorld3(206.1);
+// const popOfVietnam = percentageOfWorld3(97.34);
+// console.log(popOfUSA, popOfNigeria, popOfVietnam);
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+
+// const describePopulation = (country, population) => {
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+//   console.log(description);
+// };
+
+// describePopulation("USA", 329);
+// describePopulation("Nigeria", 206);
+// describePopulation("Vietnam", 97);
+
+// const populations = [329, 206, 97, 105];
+// console.log(populations.length === 4);
+
+// const percentages = [
+//   percentageOfWorld1(populations[0]),
+//   percentageOfWorld1(populations[1]),
+//   percentageOfWorld1(populations[2]),
+//   percentageOfWorld1(populations[3]),
+// ];
+
+// console.log(percentages);
+
+//-------------------------------------------------------------------------
+
+//CODING CHALLENGES
+
+//Coding Challenge #1
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// const avgDolphins = calcAverage(85, 54, 49);
+// const avgKoalas = calcAverage(23, 34, 27);
+
+// const checkWinner = function (avg1, avg2) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log("No one wins!");
+//   }
+// };
+
+// checkWinner(avgDolphins, avgKoalas);
