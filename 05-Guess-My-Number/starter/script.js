c1ba502353789ee591c1954fed250602ce22e77b
Erin Schallmoser <dialogofadream@gmail.com>
2021-11-18 21:40:15 -0800

    add event listener function to Guess My Number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,25 +13,27 @@
 
 //Handling Click Events, Implementing Game Logic
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
 let highScore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.number').textContent = secretNumber;
-  document.querySelector('.score').textContent = score;
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = 'Start guessing...';
-  //   document.querySelector('.number').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  console.log(secretNumber);
 
   //when there is no input
   if (!guess) {
@@ -39,9 +41,14 @@ document.querySelector('.check').addEventListener('click', function () {
 
     //when player wins
   } else if (guess === secretNumber) {
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     document.querySelector('.message').textContent = 'Correct Number!🎉';
-    highScore = score;
-    document.querySelector('.highscore').textContent = highScore;
+    document.querySelector('.number').textContent = secretNumber;
+    // highScore = score;
+    // document.querySelector('.highscore').textContent = highScore;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
