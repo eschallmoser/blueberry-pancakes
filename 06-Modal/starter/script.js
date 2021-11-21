b142d659eb73a30e86042dd06d3b4e8ac3b0cc4c
Erin Schallmoser <dialogofadream@gmail.com>
2021-11-20 18:40:55 -0800

    add event listeners to elements in Modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,5 +3,35 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.open-modal');
-console.log(showModal);
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openOrCloseModal = function () {
+  if (modal.classList.contains('hidden')) {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  } else {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openOrCloseModal);
+}
+
+btnCloseModal.addEventListener('click', openOrCloseModal);
+overlay.addEventListener('click', openOrCloseModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    openOrCloseModal();
+  }
+});
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'c' && !modal.classList.contains('secret')) {
+    modal.classList.add('secret');
+  } else if (event.key === 'c' && modal.classList.contains('secret')) {
+    modal.classList.remove('secret');
+  }
+});
